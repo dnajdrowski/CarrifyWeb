06f194290363f3e627e0389063ecbe994b7b4be5
posialero <danielnajdrowski@gmail.com>
2019-11-03 13:13:53 +0100

    Added SpringBoot project.

---
+++ src/main/java/com/carrify/web/carrifyweb/CarrifyWebApplication.java
@@ -0,0 +1,13 @@
+package com.carrify.web.carrifyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrifyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrifyWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carrify/web/carrifyweb/CarrifyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrify.web.carrifyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrifyWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
