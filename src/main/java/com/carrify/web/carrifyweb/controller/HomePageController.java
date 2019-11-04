d11d0485e2c72cecf96c0e980901b43b856e1a27
krzysztofserocki <leicer131@gmail.com>
2019-11-04 20:28:27 +0100

    api fix

--- src/main/java/com/carrify/web/carrifyweb/controller/HomePageController.java
+++ src/main/java/com/carrify/web/carrifyweb/controller/HomePageController.java
@@ -1,9 +1,7 @@
 package com.carrify.web.carrifyweb.controller;
 
 import net.minidev.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HomePageController {
 
-    @GetMapping({"", "/", "/hello"})
-    public ResponseEntity<String> homePage(@RequestParam(required = false, defaultValue = "World") String name) {
+    @PostMapping(value = "/hello", produces = "application/json")
+    public String homePage(
+            @RequestParam(required = false, defaultValue = "World") String name) {
         JSONObject object = new JSONObject();
         object.put("name", name);
-        return new ResponseEntity<>(object.toString(), HttpStatus.OK);
+        object.put("surname", name + " " + name);
+        return object.toString();
     }
 }
