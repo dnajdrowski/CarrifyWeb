a3d7676eda67c11cd53a45c4dd4e161c595c82e3
posialero <danielnajdrowski@gmail.com>
2019-11-05 23:41:12 +0100

    Add GET endpoint for simple testing Heroku auto-deploy. Changed Java Version from 8 to 11.

--- src/main/java/com/carrify/web/carrifyweb/controller/HomePageController.java
+++ src/main/java/com/carrify/web/carrifyweb/controller/HomePageController.java
@@ -1,21 +1,25 @@
 package com.carrify.web.carrifyweb.controller;
 
 import net.minidev.json.JSONObject;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class HomePageController {
 
     @PostMapping(value = "/hello", produces = "application/json")
-    public String homePage(
+    public String helloPage(
             @RequestParam(required = false, defaultValue = "World") String name) {
         JSONObject object = new JSONObject();
         object.put("name", name);
         object.put("surname", name + " " + name);
+        return object.toString();
+    }
+
+    @GetMapping({"", "/"})
+    public String homePage() {
+        JSONObject object = new JSONObject();
+        object.put("appname", "carrify-web");
         return object.toString();
     }
 }
