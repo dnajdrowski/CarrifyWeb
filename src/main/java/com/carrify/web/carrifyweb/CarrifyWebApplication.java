6f66945d3493a6e7319053538991ec505af36fbc
dnajdrowski <danielnajdrowski@gmail.com>
2019-12-11 23:12:46 +0100

    Setting default TimeZone.

--- src/main/java/com/carrify/web/carrifyweb/CarrifyWebApplication.java
+++ src/main/java/com/carrify/web/carrifyweb/CarrifyWebApplication.java
@@ -3,11 +3,21 @@ package com.carrify.web.carrifyweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@EnableScheduling
 public class CarrifyWebApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     @Bean
     public PasswordEncoder bCryptPasswordEncoder() {
